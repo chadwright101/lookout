9cc536b7bfe1844029181cc30d130cc9dde3edbc
Chad Wright <justdeejays@gmail.com>
2025-06-20 12:02:11 +0200

    feat: add updated jiwu info

--- _components/pages/home-page/our-stores/truncated-paragraph.tsx
+++ _components/pages/home-page/our-stores/truncated-paragraph.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { X } from "lucide-react";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
 interface TruncatedParagraphProps {
   paragraphs?: string[];
